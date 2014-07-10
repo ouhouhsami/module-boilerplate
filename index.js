acae7316bae9c7577c9d1734434ed39029304051
Emmanuel Freard <e.freard@vi-live.fr>
2014-07-10 12:41:32 +0200

    update with new tasks

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ module.exports = function(gulp, packageJson) {
   var tools = {};
   
   //replace for [cdn] dynamic value and check http in url name
-  tools.processCdn = function(options) {
-    for(var i in options.css) {
-        options.css[i] = options.css[i].replace(/\[cdn\]/g, options.cdn);
-        if(options.js[i].substr(0, 2) == '//')
-            options.js[i] = 'http:' + options.js[i];
-    }
-    for(var j in options.js) {
-        options.js[j] = options.js[j].replace(/\[cdn\]/g, options.cdn);
-        if(options.js[j].substr(0, 2) == '//')
-            options.js[j] = 'http:' + options.js[j];
-    }
+  tools.getOptionUrl = function(option, cdn) {
+    if(option.ircamlib)  
+      option.url = "/Ircam-RnD/" + option.url + "/master/" + option.url + ".js";
+    if(option.cdn)
+      option.url = cdn + option.url;
+    if(option.url.substr(0, 2) == '//')
+      option.url = 'http:' + option.url;
+    return option.url;
   }
+  
+  tools.processOptions = function(options) {
+    options.internalCSS = [];
+    options.externalCSS = [];
+    options.internalJS = [];
+    options.externalJS = [];
+    for(var i in options.css.internal)
+        options.internalCSS.push(tools.getOptionUrl(options.css.internal[i], options.cdn));
+    for(var i in options.css.external)
+        options.externalCSS.push(tools.getOptionUrl(options.css.external[i], options.cdn));
+    for(var i in options.js.internal)
+        options.internalJS.push(tools.getOptionUrl(options.js.internal[i], options.cdn));
+    for(var i in options.js.external)
+        options.externalJS.push(tools.getOptionUrl(options.js.external[i], options.cdn));
+  }
+  
+  tools.boilerplatePath = './node_modules/module-boilerplate/';
   
   var browserify = require('browserify');
   var uglify = require('gulp-uglify');
@@ -26,8 +40,10 @@ module.exports = function(gulp, packageJson) {
   var gutil = require("gulp-util");
   var download = require("gulp-download");
   var conflict = require("gulp-conflict");
+  var clean = require("gulp-clean");
   var runSequence = require("run-sequence");
   var fs = require('fs');
+  var path = require('path');
   
   try {
     var options = require('../../docs/options.json');
@@ -36,12 +52,9 @@ module.exports = function(gulp, packageJson) {
     gutil.log(gutil.colors.yellow("docs/options.json does not exist execute"), gutil.colors.cyan("gulp init-docs"));
   }
   
-
   var libName = packageJson.exports || packageJson.name;
   var exports =  packageJson.exports;
   var dependencies = Object.keys(packageJson && packageJson.dependencies || {});
-  
-  
   
   //lib with dependencies
   gulp.task('standalone', function () {
@@ -95,6 +108,15 @@ module.exports = function(gulp, packageJson) {
   
   //Generation of specific docs files for the repo
   gulp.task('init-docs', function() {
+    
+    fs.mkdir("./docs", function(e) {
+      if(!e || (e && e.code === 'EEXIST')){
+        gutil.log("The folder /docs was created!");
+      } else {
+        gutil.log(e);
+      }
+    });
+    
     gulp.src(['node_modules/module-boilerplate/docs/options.json'])
       .pipe(verb({
         dest: 'options.json',
@@ -126,67 +148,130 @@ module.exports = function(gulp, packageJson) {
     }); 
   });
   
-  //get remote files to the doc repositories
-  gulp.task('dl-docs-files', function() {
-    tools.processCdn(options);
-    
-    download(options.css)
-      .pipe(gulp.dest("./docs/css"));
-
-    download(options.js)
-      .pipe(gulp.dest("./docs/js"));
-  });
-  
   gulp.task('get-default-tpl', function() {
-    return gulp.src(['./node_modules/module-boilerplate/docs/README.tmpl.md', './node_modules/module-boilerplate/docs/index.tmpl.html'])
-      .pipe(gulp.dest('./_tmpdocs/'));
+    return gulp.src([tools.boilerplatePath + 'docs/README.tmpl.md', tools.boilerplatePath + 'docs/index.tmpl.html'])
+      .pipe(gulp.dest(tools.boilerplatePath + '_tmpdocs/'));
   });
   
   gulp.task('get-default-partials', function() {
-      return gulp.src('./node_modules/module-boilerplate/docs/partials/*')
-      .pipe(gulp.dest('./_tmpdocs/partials/'));
+      return gulp.src(tools.boilerplatePath + 'docs/partials/*')
+      .pipe(gulp.dest(tools.boilerplatePath + '_tmpdocs/partials/'));
   });
   
   gulp.task('get-repo-tpl', function() {
     return gulp.src(['./docs/README.tmpl.md', './docs/index.tmpl.html'])
-      .pipe(conflict('./_tmpdocs/', {cwd : './_tmpdocs/todel'}))
-      .pipe(gulp.dest('./_tmpdocs/'));
+      .pipe(conflict(tools.boilerplatePath + '_tmpdocs/', {cwd : tools.boilerplatePath + 'todel'}))
+      .pipe(gulp.dest(tools.boilerplatePath + '_tmpdocs/'));
   });
   
   gulp.task('get-repo-partials', function() {
     return gulp.src('./docs/partials/*')
-      .pipe(conflict('./_tmpdocs/partials/', {cwd : './_tmpdocs/todel'}))
-      .pipe(gulp.dest('./_tmpdocs/partials/'));
+      .pipe(conflict(tools.boilerplatePath + '_tmpdocs/partials/', {cwd : tools.boilerplatePath + 'todel'}))
+      .pipe(gulp.dest(tools.boilerplatePath + '_tmpdocs/partials/'));
   });
   
   //Generation of README.md
   gulp.task('verb-docs', function() {
-    return gulp.src(['_tmpdocs/README.tmpl.md'])
+    return gulp.src([tools.boilerplatePath + '_tmpdocs/README.tmpl.md'])
       .pipe(verb({
         dest: 'README.md'
       }))
       .pipe(gulp.dest('./'));
   });
-
+  
+  // Start internal task for create gh-pages
+  gulp.task('verb-gh-pages-snd', function() {
+    return gulp.src('./docs/snd/*')
+      .pipe(gulp.dest('./gh-pages/snd/'));
+  });
+    
+  gulp.task('verb-gh-pages-datas', function() {
+    return gulp.src('./docs/datas/*')
+      .pipe(gulp.dest('./gh-pages/datas/'));
+  });      
+  
+  gulp.task('verb-gh-pages-utils', function() {
+    return gulp.src('./docs/utils/*')
+      .pipe(gulp.dest('./gh-pages/utils/'));
+  });
+  
+  gulp.task('verb-gh-pages-lib', function() {
+    return gulp.src('./build/' + packageJson.name + '.js')
+      .pipe(gulp.dest('./gh-pages/js/'));
+  });
+  
+  gulp.task('process-options', function() {
+    tools.processOptions(options);
+    return true;
+  });
+  
+  //Download internal JS files
+  gulp.task('verb-gh-pages-dl-internal-js', function() {
+    if(options.internalJS.length > 0) {
+      return download(options.internalJS)
+        .pipe(gulp.dest("./gh-pages/js"));
+    }
+    else
+      return true;
+  });
+  
+  //Download internal CSS files
+  gulp.task('verb-gh-pages-dl-internal-css', function() {
+    if(options.internalCSS.length > 0) {
+      return download(options.internalCSS)
+        .pipe(gulp.dest("./gh-pages/css"));
+    }
+  });
+  
   //Generation of github page
   gulp.task('verb-gh-pages', function() {
+  
+    for(var i in options.internalJS)
+      options.internalJS[i] = path.basename(options.internalJS[i]);
+    for(var i in options.internalCSS)
+      options.internalCSS[i] = path.basename(options.internalCSS[i]);
     
-    if(!options.external.css) {
-      gulp.src('./docs/css/**/*', {base: './docs/'})
-        .pipe(gulp.dest('./gh-pages/'));
-    }
+    gutil.log(options.internalCSS);
     
-    if(!options.external.js) {
-      gulp.src('./docs/js/**/*', {base: './docs/'})
-        .pipe(gulp.dest('./gh-pages/'));
-    }
-    
-    gulp.src(['./_tmpdocs/index.tmpl.html'])
+    return gulp.src([tools.boilerplatePath + '_tmpdocs/index.tmpl.html'])
       .pipe(verb({
-        name: libName,
+        options : options,
         dest: 'index.html'
       }))
       .pipe(gulp.dest('./gh-pages'));
+  });
+  
+  gulp.task('update-demo-path', function() {
+    return gulp.src('./gh-pages/**')
+      .pipe(gulp.dest('./examples/'));
+  });
+  
+  gulp.task('launch-server-gh-page', function() {
+    gutil.log(gutil.colors.green("Test your gh-pages on 9002 port"));
+    connect.server({
+      root: ['./gh-pages'],
+      port: 9002,
+      livereload: false
+    });
+  });
+  
+  gulp.task('launch-server-demo-path', function() { 
+    gutil.log(gutil.colors.green("Test your demo path on 9003 port"));
+    return connect.server({
+      root: ['./examples'],
+      port: 9003,
+      livereload: false
+    });
+  });
+  
+  gulp.task('clean-gh-pages', function() {
+    return gulp.src('./gh-pages/', {read: false})
+        .pipe(clean());
+  });
+  
+  gulp.task('clean-after', function() {
+    return gulp.src(tools.boilerplatePath + '_tmpdocs/', {read: false})
+        .pipe(clean());
   });
 
   gulp.task('default', ['standalone', 'uglify']);
@@ -198,10 +283,30 @@ module.exports = function(gulp, packageJson) {
   gulp.task('test', ['dependencies', 'lib', 'connectDev' , 'watch']);
 
   gulp.task('docs', function(callback) {
-    runSequence('get-default-tpl', 'get-default-partials', 'get-repo-tpl', 'get-repo-partials', 'verb-docs', callback);
+    runSequence('get-default-tpl', 'get-default-partials', 'get-repo-tpl', 'get-repo-partials', 'verb-docs', 'clean-after', callback);
   });
-
-  gulp.task('gh-pages', ['verb-gh-pages']);
+  
+  //Generation of github pages
+  //delete the path before
+  //mix repo content and boilerplate content
+  //get static content like snd, datas and utils
+  //procces the css and js dependencies
+  //download internal css and js dependencies
+  //create index.html and test with the local server
+  //delete tmp files
+  gulp.task('gh-pages', function(callback) {
+    runSequence(
+      'clean-gh-pages', 'get-default-tpl', 'get-default-partials', 'get-repo-tpl', 'get-repo-partials', 
+      'verb-gh-pages-snd','verb-gh-pages-datas', 'verb-gh-pages-utils', 'verb-gh-pages-lib', 
+      'process-options', 'verb-gh-pages-dl-internal-js', 'verb-gh-pages-dl-internal-css', 
+      'verb-gh-pages', 'launch-server-gh-page', 'clean-after',
+    callback);
+  });
+  
+  //update demo from gh-pages but no clean the example folder before
+  gulp.task('update-demo', function(callback) {
+    runSequence('gh-pages', 'update-demo-path', 'launch-server-demo-path');
+  });
   
   //Deploy github page to the master on gh-pages branch 
   gulp.task('deploy-gh-pages', function() {
